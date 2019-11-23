1d75bcfd48d43861bbe3211763db57908f38997e
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-11-23 16:56:16 +0330

    created class DataReader

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,3 +5,17 @@ Created on Sat Nov 23 16:52:45 2019
 @author: Mahdi
 """
 
+class DataReader():
+    """
+    
+    """
+    def __init__(self):
+        pass
+
+    def Load_Data(self):
+        pass
+
+    def Tokenizer(self):
+        pass
+
+    
