08512e039192eca263629754ae4d7fd65410fe61
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-11-23 17:06:14 +0330

    updated the init function and also must-be-imported packages!

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,13 +4,24 @@ Created on Sat Nov 23 16:52:45 2019
 
 @author: Mahdi
 """
+import os 
+import re
+from os.path import splitext, split
+
 
 class DataReader():
     """
     
     """
-    def __init__(self):
-        pass
+    def __init__(self, loading_dir, saving_dir, fformat):
+        self.loading_dir = loading_dir
+        self.saving_dir = saving_dir
+        self.fformat = fformat   # imported file format
+                                # pls note that file format has to be 
+                                # double checked in the future to see
+                                # if the imported format is valid for 
+                                # the application.
+
 
     def Load_Data(self):
         pass
